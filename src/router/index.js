1208cbe46e2f6ebd637c43a33272125ec942d963
m <dev.cheuk.lee@gmail.com>
2019-09-30 18:17:37 -0400

    added History route

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import {Switch, Route} from "react-router-dom";
 // Views
 import Home from '../views/Home';
 import Settings from '../views/Settings';
+import History from '../views/History';
 
 const routes = (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/settings" component={Settings} />
+                <Route exact path="/history" component={History} />
             </Switch>
     );
 
