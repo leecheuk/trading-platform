d09a21e69a26816731ea887f98f8d45ed70eacb1
m <dev.cheuk.lee@gmail.com>
2019-09-30 22:20:12 -0400

    added main layout

--- src/views/History.js
+++ src/views/History.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 function History() {
     return (
-        <h1>History</h1>
+        <Layout>
+            <h1>History</h1>
+        </Layout>
     )
 }
 
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 function Settings() {
     return (
-        <h1>Settings</h1>
+        <Layout>
+            <h1>Settings</h1>
+        </Layout>
     )
 }
 
