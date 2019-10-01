0e1b5ab16fba7eb62b9d5d988823b61e463297e9
m <dev.cheuk.lee@gmail.com>
2019-09-30 22:19:19 -0400

    added active class

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import {useHistory} from "react-router";
 import {Link} from "react-router-dom";
 
 function Sidebar() {
+    const history = useHistory();
+    const path = history.location.pathname;
     return (
         <ul className="side-bar">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/history">History</Link></li>
-            <li><Link to="/settings">Settings</Link></li>
+            <li><Link to="/" className={path == "/" ? "active" : null}>Home</Link></li>
+            <li><Link to="/history" className={path == "/history" ? "active" : null}>History</Link></li>
+            <li><Link to="/settings" className={path == "/settings" ? "active" : null}>Settings</Link></li>
         </ul>
     )
 }
